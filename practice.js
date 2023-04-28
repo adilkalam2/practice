a964d587584be2d2804d90c11624c63ceec51c9e
adilkalam <adilh173@gmail.com>
2023-04-29 02:04:41 +0530

    practice one

---
+++ practice.js
@@ -0,0 +1,30 @@
+import 'bulma/css/bulma.css';
+import ProfileCard from './profileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+function App() {
+    return (
+        <div>
+            <div>Personal Digital Assistant</div>
+
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-2">
+                        <ProfileCard title= "Alexa" handle= "@alexa" image= {AlexaImage}/>
+                        </div>
+                        <div className="column is-2">
+                        <ProfileCard title= "Siri" handle= "@siri" image= {CortanaImage}/>
+                        </div>
+                        <div className="column is-2">
+                        <ProfileCard title= "Cortana" handle= "@cortana" image= {SiriImage}/>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            
+        </div>
+    );
+}
+export default App;
